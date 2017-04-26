d6e4a9efae27ac2e5017bb57833372435aed4d0d
Aktorius <miri.tarik@gmail.com>
2017-04-26 22:37:22 +0100

    Implement LoginPresenter contract

--- app/src/main/java/aktorius/com/android/chapp/contracts/LoginPresenter.java
+++ app/src/main/java/aktorius/com/android/chapp/contracts/LoginPresenter.java
@@ -1,5 +1,7 @@
 package aktorius.com.android.chapp.contracts;
 
+import aktorius.com.android.chapp.events.LoginEvent;
+
 /**
  * Created by Aktorius on 26/04/2017.
  */
@@ -8,6 +10,7 @@ public interface LoginPresenter {
     void onCreate();
     void onDestroy();
     void checkForAuthenticatedUser();
+    void onEventMainThread(LoginEvent event);
     void validateLogin(String email, String password);
     void registerNewUser(String email, String password);
 }
