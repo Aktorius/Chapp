8db1ca02f2b55251d37e51d7595ab3b8edfe2a67
Aktorius <miri.tarik@gmail.com>
2017-04-27 21:03:07 +0100

    Create backbone of ContactListActivity

--- app/src/main/java/aktorius/com/android/chapp/activities/ContactListActivity.java
+++ app/src/main/java/aktorius/com/android/chapp/activities/ContactListActivity.java
@@ -1,10 +1,17 @@
 package aktorius.com.android.chapp.activities;
 
+import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 
+import aktorius.com.android.chapp.R;
 import aktorius.com.android.chapp.contracts.ContactListView;
 import aktorius.com.android.chapp.contracts.OnItemClickListener;
 import aktorius.com.android.chapp.domain.User;
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 /**
  * Created by Aktorius on 27/04/2017.
@@ -12,7 +19,40 @@ import aktorius.com.android.chapp.domain.User;
 
 public class ContactListActivity extends AppCompatActivity
                                  implements ContactListView, OnItemClickListener{
-    
+
+    //UI References
+    @BindView(R.id.toolbar)
+    Toolbar toolbar;
+    //@BindView(R.id.rvContacts)
+    //RecyclerView rvContacts;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_contact_list);
+        ButterKnife.bind(this);
+    }
+
+    @Override
+    protected void onResume(){
+        super.onResume();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+    }
+
+    @OnClick(R.id.fab)
+    public void addContact(){
+        //TODO: Action when pressing the fab
+    }
+
     @Override
     public void onContactAdded(User user) {
 
