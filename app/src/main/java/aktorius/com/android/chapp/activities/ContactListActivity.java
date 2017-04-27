5dd04c5a6d07a48d716497f952cfbda5b04856d2
Aktorius <miri.tarik@gmail.com>
2017-04-27 22:50:00 +0100

    Implement ContactListActivity

--- app/src/main/java/aktorius/com/android/chapp/activities/ContactListActivity.java
+++ app/src/main/java/aktorius/com/android/chapp/activities/ContactListActivity.java
@@ -3,12 +3,21 @@ package aktorius.com.android.chapp.activities;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 
+import java.util.ArrayList;
+
+import aktorius.com.android.chapp.Chapp;
 import aktorius.com.android.chapp.R;
+import aktorius.com.android.chapp.adapters.ContactListAdapter;
+import aktorius.com.android.chapp.contracts.ContactListPresenter;
 import aktorius.com.android.chapp.contracts.ContactListView;
 import aktorius.com.android.chapp.contracts.OnItemClickListener;
 import aktorius.com.android.chapp.domain.User;
+import aktorius.com.android.chapp.libraries.ImageLoader;
+import aktorius.com.android.chapp.services.contactlist.ContactListPresenterImpl;
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
@@ -23,14 +32,26 @@ public class ContactListActivity extends AppCompatActivity
     //UI References
     @BindView(R.id.toolbar)
     Toolbar toolbar;
-    //@BindView(R.id.rvContacts)
-    //RecyclerView rvContacts;
+    @BindView(R.id.rvContacts)
+    RecyclerView rvContacts;
+
+    private ContactListAdapter adapter;
+    private ContactListPresenter contactListPresenter;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_contact_list);
         ButterKnife.bind(this);
+
+        contactListPresenter = new ContactListPresenterImpl(this);
+        contactListPresenter.onCreate();
+
+        toolbar.setSubtitle(contactListPresenter.getCurrentUserEmail());
+        setSupportActionBar(toolbar);
+
+        setupAdapter();
+        setupRecyclerView();
     }
 
     @Override
@@ -53,28 +74,39 @@ public class ContactListActivity extends AppCompatActivity
         //TODO: Action when pressing the fab
     }
 
+    private void setupAdapter() {
+        Chapp app = (Chapp) getApplication();
+        ImageLoader imageLoader = app.getImageLoader();
+        adapter = new ContactListAdapter(new ArrayList<User>(), imageLoader, this);
+    }
+
+    private void setupRecyclerView() {
+        rvContacts.setLayoutManager(new LinearLayoutManager(this));
+        rvContacts.setAdapter(adapter);
+    }
+
     @Override
     public void onContactAdded(User user) {
-
+        adapter.add(user);
     }
 
     @Override
     public void onContactChanged(User user) {
-
+        adapter.update(user);
     }
 
     @Override
     public void onContactRemoved(User user) {
-
+        adapter.remove(user);
     }
 
     @Override
     public void onItemClick(User user) {
-
+        //TODO: start a chat
     }
 
     @Override
     public void onItemLongClick(User user) {
-
+        contactListPresenter.removeContact(user.getEmail());
     }
 }
