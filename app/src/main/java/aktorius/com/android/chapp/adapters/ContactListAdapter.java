cd00600a7b8b79eff9c354820c1282ee8c3709b5
Aktorius <miri.tarik@gmail.com>
2017-04-27 21:46:27 +0100

    Complete adapter implementation for contact list recycler view

--- app/src/main/java/aktorius/com/android/chapp/adapters/ContactListAdapter.java
+++ app/src/main/java/aktorius/com/android/chapp/adapters/ContactListAdapter.java
@@ -1,5 +1,6 @@
 package aktorius.com.android.chapp.adapters;
 
+import android.graphics.Color;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -11,6 +12,7 @@ import java.util.List;
 import aktorius.com.android.chapp.R;
 import aktorius.com.android.chapp.contracts.OnItemClickListener;
 import aktorius.com.android.chapp.domain.User;
+import aktorius.com.android.chapp.helpers.AvatarHelper;
 import aktorius.com.android.chapp.libraries.ImageLoader;
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -43,7 +45,19 @@ public class ContactListAdapter extends RecyclerView.Adapter<ContactListAdapter.
 
     @Override
     public void onBindViewHolder(ViewHolder holder, int position) {
+        User user = contactList.get(position);
+        holder.setClickListener(user, clickListener);
 
+        String email = user.getEmail();
+        boolean online = user.isOnline();
+        String status = online ? "online" : "offline";
+        int color = online ? Color.GREEN : Color.RED;
+
+        holder.txtUser.setText(email);
+        holder.txtStatus.setText(status);
+        holder.txtStatus.setTextColor(color);
+
+        imageLoader.load(holder.imgAvatar, AvatarHelper.getAvatarUrl(email));
     }
 
     @Override
@@ -51,6 +65,49 @@ public class ContactListAdapter extends RecyclerView.Adapter<ContactListAdapter.
         return contactList.size();
     }
 
+    public int getPositionByUsername(String username) {
+        int position = 0;
+        for (User user : contactList) {
+            if (user.getEmail().equals(username)) {
+                break;
+            }
+            position++;
+        }
+
+        return position;
+    }
+
+    private boolean alreadyInAdapter(User newUser){
+        boolean alreadyInAdapter = false;
+        for (User user : this.contactList) {
+            if (user.getEmail().equals(newUser.getEmail())) {
+                alreadyInAdapter = true;
+                break;
+            }
+        }
+
+        return alreadyInAdapter;
+    }
+
+    public void add(User user) {
+        if (!alreadyInAdapter(user)) {
+            this.contactList.add(user);
+            this.notifyDataSetChanged();
+        }
+    }
+
+    public void update(User user) {
+        int pos = getPositionByUsername(user.getEmail());
+        contactList.set(pos, user);
+        this.notifyDataSetChanged();
+    }
+
+    public void remove(User user) {
+        int pos = getPositionByUsername(user.getEmail());
+        contactList.remove(pos);
+        this.notifyDataSetChanged();
+    }
+    
     static class ViewHolder extends RecyclerView.ViewHolder{
         @BindView(R.id.imgAvatar)
         CircleImageView imgAvatar;
@@ -65,6 +122,24 @@ public class ContactListAdapter extends RecyclerView.Adapter<ContactListAdapter.
             super(itemView);
             this.itemView = itemView;
             ButterKnife.bind(this, itemView);
+        }
+
+        public void setClickListener(final User user,
+                                     final OnItemClickListener listener) {
+            itemView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    listener.onItemClick(user);
+                }
+            });
+
+            itemView.setOnLongClickListener(new View.OnLongClickListener() {
+                @Override
+                public boolean onLongClick(View view) {
+                    listener.onItemLongClick(user);
+                    return false;
+                }
+            });
         }
     }
 }
