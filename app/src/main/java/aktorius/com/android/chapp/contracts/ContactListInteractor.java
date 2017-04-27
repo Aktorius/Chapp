c5e88b2bbbc88bbced2bf73f674c572434d97c58
Aktorius <miri.tarik@gmail.com>
2017-04-27 22:35:05 +0100

    Create missing contract for contact list

---
+++ app/src/main/java/aktorius/com/android/chapp/contracts/ContactListInteractor.java
@@ -0,0 +1,12 @@
+package aktorius.com.android.chapp.contracts;
+
+/**
+ * Created by Aktorius on 27/04/2017.
+ */
+
+public interface ContactListInteractor {
+    void subscribeForContactEvents();
+    void unSubscribeForContactEvents();
+    void destroyContactListListener();
+    void removeContact(String email);
+}
