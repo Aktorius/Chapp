c904c654e68edf62fae817a54f0cdbef4ba22d57
Aktorius <miri.tarik@gmail.com>
2017-05-02 16:25:08 +0100

    Implement the chat activity

--- app/src/main/java/aktorius/com/android/chapp/activities/ChatActivity.java
+++ app/src/main/java/aktorius/com/android/chapp/activities/ChatActivity.java
@@ -1,8 +1,127 @@
 package aktorius.com.android.chapp.activities;
 
+import android.content.Intent;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+import aktorius.com.android.chapp.Chapp;
+import aktorius.com.android.chapp.R;
+import aktorius.com.android.chapp.adapters.ChatAdapter;
+import aktorius.com.android.chapp.contracts.chat.ChatPresenter;
+import aktorius.com.android.chapp.contracts.chat.ChatView;
+import aktorius.com.android.chapp.domain.ChatMessage;
+import aktorius.com.android.chapp.helpers.AvatarHelper;
+import aktorius.com.android.chapp.libraries.ImageLoader;
+import aktorius.com.android.chapp.services.chat.ChatPresenterImpl;
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+import de.hdodenhof.circleimageview.CircleImageView;
+
 /**
  * Created by Aktorius on 02/05/2017.
  */
 
-public class ChatActivity {
+public class ChatActivity extends AppCompatActivity
+                            implements ChatView{
+    @BindView(R.id.toolbar)
+    Toolbar toolbar;
+    @BindView(R.id.txtUser)
+    TextView txtUser;
+    @BindView(R.id.txtStatus)
+    TextView txtStatus;
+    @BindView(R.id.editTxtMessage)
+    EditText inputMessage;
+    @BindView(R.id.messageRecyclerView)
+    RecyclerView recyclerView;
+    @BindView(R.id.imgAvatar)
+    CircleImageView imgAvatar;
+
+    public final static String EMAIL_KEY = "email";
+    public final static String ONLINE_KEY = "online";
+
+    private ChatAdapter adapter;
+    private ChatPresenter chatPresenter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_chat);
+        ButterKnife.bind(this);
+
+        chatPresenter = new ChatPresenterImpl(this);
+        chatPresenter.onCreate();
+
+        setSupportActionBar(toolbar);
+        Intent intent = getIntent();
+        setToolbarData(intent);
+
+        setupAdapter();
+        setupRecyclerView();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        chatPresenter.onResume();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        chatPresenter.onPause();
+    }
+
+    @Override
+    protected void onDestroy() {
+        chatPresenter.onDestroy();
+        super.onDestroy();
+    }
+
+    private void setToolbarData(Intent i) {
+        String recipient = i.getStringExtra(EMAIL_KEY);
+        chatPresenter.setChatRecipient(recipient);
+
+        boolean online = i.getBooleanExtra(ONLINE_KEY, false);
+        String status = online ? "online" : "offline";
+        int color = online ? Color.GREEN : Color.RED;
+
+        txtUser.setText(recipient);
+        txtStatus.setText(status);
+        txtStatus.setTextColor(color);
+
+        Chapp app = (Chapp) getApplication();
+        ImageLoader imageLoader = app.getImageLoader();
+        imageLoader.load(imgAvatar, AvatarHelper.getAvatarUrl(recipient));
+    }
+
+    private void setupAdapter() {
+        adapter = new ChatAdapter(this, new ArrayList<ChatMessage>());
+    }
+
+    private void setupRecyclerView() {
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        recyclerView.setAdapter(adapter);
+    }
+
+    @Override
+    @OnClick(R.id.btnSendMessage)
+    public void sendMessage() {
+        chatPresenter.sendMessage(inputMessage.getText().toString());
+        inputMessage.setText("");
+    }
+
+    @Override
+    public void onMessageReceived(ChatMessage msg) {
+        adapter.add(msg);
+        recyclerView.scrollToPosition(adapter.getItemCount() - 1);
+    }
 }
